5afe1d6a3505f0438248f2aa25a5e7a32dc1efa1
xafero <georg.berendt@uni-oldenburg.de>
2015-08-18 02:27:04 +0200

    Added test stubs

---
+++ src/test/java/com/xafero/kitea/collections/ObservablesTest.java
@@ -0,0 +1,38 @@
+package com.xafero.kitea.collections;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ObservablesTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void testDecorateIterableOfT() {
+	}
+
+	@Test
+	public void testDecorateCollectionOfT() {
+	}
+
+	@Test
+	public void testDecorateSetOfT() {
+	}
+
+	@Test
+	public void testDecorateListOfT() {
+	}
+
+	@Test
+	public void testDecorateMapOfKV() {
+	}
+}
