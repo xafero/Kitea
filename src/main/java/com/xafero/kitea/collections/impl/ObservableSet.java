19ddd3089b6a62feee21ac67c8426dd4f151c515
xafero <georg.berendt@uni-oldenburg.de>
2015-08-18 22:17:45 +0200

    Corrected types

--- src/main/java/com/xafero/kitea/collections/impl/ObservableSet.java
+++ src/main/java/com/xafero/kitea/collections/impl/ObservableSet.java
@@ -1,13 +1,11 @@
 package com.xafero.kitea.collections.impl;
 
-import java.util.Collection;
 import java.util.Set;
 
-public class ObservableSet<T> extends ObservableCollection<T>implements Set<T> {
+public class ObservableSet<T> extends ObservableCollection<T> implements Set<T> {
 
-	public ObservableSet(Collection<T> collection) {
-		super(collection);
-		// TODO Auto-generated constructor stub
+	public ObservableSet(Set<T> set) {
+		super(set);
 	}
 
 	public static <E> ObservableSet<E> wrap(Set<E> set) {
