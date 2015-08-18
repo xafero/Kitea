8a1379abc474eb12260b05a94b99c5fe94ad27ab
xafero <georg.berendt@uni-oldenburg.de>
2015-08-18 02:32:48 +0200

    Some testing

--- src/test/java/com/xafero/kitea/collections/ObservablesTest.java
+++ src/test/java/com/xafero/kitea/collections/ObservablesTest.java
@@ -2,37 +2,49 @@ package com.xafero.kitea.collections;
 
 import static org.junit.Assert.*;
 
+import java.util.*;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.xafero.kitea.collections.impl.*;
+import com.xafero.kitea.collections.impl.ObservableIterable;
+
 public class ObservablesTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
+	@Test
+	public void testDecorateIterable() {
+		Iterable<String> iterable = new LinkedList<String>(Arrays.asList("Hello", "World"));
+		ObservableIterable<String> observe = Observables.decorate(iterable);
+		assertNotNull(observe);
 	}
 
 	@Test
-	public void testDecorateIterableOfT() {
+	public void testDecorateCollection() {
+		Collection<String> collection = new LinkedList<String>(Arrays.asList("Hello", "World"));
+		ObservableCollection<String> observe = Observables.decorate(collection);
+		assertNotNull(observe);
 	}
 
 	@Test
-	public void testDecorateCollectionOfT() {
+	public void testDecorateSet() {
+		Set<String> set = new HashSet<String>(Arrays.asList("Hello", "World"));
+		ObservableSet<String> observe = Observables.decorate(set);
+		assertNotNull(observe);
 	}
 
 	@Test
-	public void testDecorateSetOfT() {
+	public void testDecorateList() {
+		List<String> list = new LinkedList<String>(Arrays.asList("Hello", "World"));
+		ObservableList<String> observe = Observables.decorate(list);
+		assertNotNull(observe);
 	}
 
 	@Test
-	public void testDecorateListOfT() {
-	}
-
-	@Test
-	public void testDecorateMapOfKV() {
+	public void testDecorateMap() {
+		Map<String, String> map = new HashMap<String, String>();
+		ObservableMap<String, String> observe = Observables.decorate(map);
+		assertNotNull(observe);
 	}
 }
